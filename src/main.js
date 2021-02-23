aee65139a26e70a0962539d54611114b36c85f01
QQH <571424332@qq.com>
2021-02-23 10:27:38 +0800

    init object successed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/router";
+import store from "./store/store";
+
+// 导入全局样式
+import "./styles/index.css";
 
 Vue.config.productionTip = false;
 
